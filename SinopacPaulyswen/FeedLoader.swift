bd602cc256e82b79de0d53582d2ed93111ae96bd
paul5130 <paul51305040@gmail.com>
2022-07-13 14:31:56 +0800

    create `FeedLoader` protocol boundary

---
+++ SinopacPaulyswen/FeedLoader.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoader.swift
+//  SinopacPaulyswen
+//
+//  Created by Paul Wen on 2022/7/13.
+//
+
+import Foundation
+
+protocol FeedLoader{
+    func load(completion: @escaping((Result<[Filtered],Error>) -> Void))
+}
