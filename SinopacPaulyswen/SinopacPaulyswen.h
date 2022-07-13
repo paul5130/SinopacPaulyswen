5864bba4d7c41448201fa797bd4488dc3c845328
paul5130 <paul51305040@gmail.com>
2022-07-13 14:16:04 +0800

    First Init

---
+++ SinopacPaulyswen/SinopacPaulyswen.h
@@ -0,0 +1,18 @@
+//
+//  SinopacPaulyswen.h
+//  SinopacPaulyswen
+//
+//  Created by Paul Wen on 2022/7/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SinopacPaulyswen.
+FOUNDATION_EXPORT double SinopacPaulyswenVersionNumber;
+
+//! Project version string for SinopacPaulyswen.
+FOUNDATION_EXPORT const unsigned char SinopacPaulyswenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SinopacPaulyswen/PublicHeader.h>
+
+
