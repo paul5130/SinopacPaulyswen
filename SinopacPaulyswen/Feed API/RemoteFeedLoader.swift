d4f92f7df17fc7751580540edca9ac2444d5f68a
paul5130 <paul51305040@gmail.com>
2022-07-14 09:40:29 +0800

    Handling Errors + Stubbing vs. Spying + Eliminating Invalid Paths

--- SinopacPaulyswen/Feed API/RemoteFeedLoader.swift
+++ SinopacPaulyswen/Feed API/RemoteFeedLoader.swift
@@ -7,18 +7,30 @@
 
 import Foundation
 
+public typealias HTTPClientResult = (Result<HTTPURLResponse,Error>)
 public protocol HTTPClient{
-    func get(from url: URL)
+    func get(from url: URL,completion: @escaping((HTTPClientResult) -> Void))
 }
 
 public final class RemoteFeedLoader{
     private let url: URL
     private let client: HTTPClient
+    public enum Error: Swift.Error{
+        case connectivity
+        case invalidData
+    }
     public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
-    public func load(){
-        client.get(from: url)
+    public func load(completion: @escaping(Error) -> Void){
+        client.get(from: url){ result in
+            switch result{
+            case .success:
+                completion(.invalidData)
+            case .failure:
+                completion(.connectivity)
+            }
+        }
     }
 }
--- SinopacPaulyswenTests/RemoteFeedLoaderTest.swift
+++ SinopacPaulyswenTests/RemoteFeedLoaderTest.swift
@@ -7,44 +7,87 @@
 import SinopacPaulyswen
 import XCTest
 
-class RemoteFeedLoaderTests: XCTestCase{
-    
+class RemoteFeedLoaderTests: XCTestCase {
+    // 測試一開始沒有任何url
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
-        
+
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
-    
+    // 測試給定的url與回來的url相同
     func test_load_requestDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
-        
-        sut.load()
-        
-        XCTAssertEqual(client.requestedURLs,[url])
+
+        sut.load { _ in }
+
+        XCTAssertEqual(client.requestedURLs, [url])
     }
-    func test_loadTwice_requestsDataFromURLTwice(){
+    // 測試讀取二次一樣的url會回來二次一樣的url
+    func test_loadTwice_requestsDataFromURLTwice() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
-        
-        sut.load()
-        sut.load()
-        
-        XCTAssertEqual(client.requestedURLs,[url,url])
+
+        sut.load { _ in }
+        sut.load { _ in }
+
+        XCTAssertEqual(client.requestedURLs, [url, url])
     }
+    // 測試傳遞錯誤的值會回應正確的錯誤資訊
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+
+        let clientError = NSError(domain: "Test", code: 0)
+        client.complete(with: clientError)
+        XCTAssertEqual(capturedErrors, [.connectivity])
+    }
+    // 回傳status code有被抓到
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        let samples = [199, 201, 300, 400, 500]
+
+        samples.enumerated().forEach { index, code in
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load { capturedErrors.append($0) }
+
+            client.complete(withStatusCode: code, at: index)
+
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
+    }
+
     // MARK: - Helpers
-    
+
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
         return (sut, client)
     }
-    
+
     private class HTTPClientSpy: HTTPClient {
-        var requestedURLs = [URL]()
-        
-        func get(from url: URL) {
-            requestedURLs.append(url)
+        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+        var requestedURLs: [URL] {
+            return messages.map { $0.url }
+        }
+
+        func get(from url: URL, completion: @escaping ((HTTPClientResult) -> Void)) {
+            messages.append((url, completion))
+        }
+
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(.failure(error))
+        }
+
+        func complete(withStatusCode code: Int, at index: Int = 0) {
+            let response = HTTPURLResponse(
+                url: requestedURLs[index],
+                statusCode: code,
+                httpVersion: nil,
+                headerFields: nil
+            )!
+            messages[index].completion(.success(response))
         }
     }
 }
